5eee185b872e52266d46e81f7c37125c3df4256e
galkowskim <mikolaj.galkowski7@gmail.com>
2022-11-19 22:04:21 +0100

    update utils and add tests

--- utils.py
+++ utils.py
@@ -12,14 +12,14 @@ BOUNDARIES = [(21.9430, -67.5), (55.7765, -135)]
 delta_latitude = BOUNDARIES[1][0] - BOUNDARIES[0][0]
 delta_longitude = BOUNDARIES[1][1] - BOUNDARIES[0][1]
 
-shape = (2565, 5119) # 2566, 5120
+shape = (2565, 5119)  # 2566, 5120
 
-def coordinatesToIdx(latitude: int, longitude: int) -> tuple[int]:
 
+def coordinates_to_idx(latitude: int, longitude: int) -> tuple[int]:
     prop_latitude = (latitude - BOUNDARIES[0][0]) / delta_latitude
     prop_longitude = (longitude - BOUNDARIES[0][1]) / delta_longitude
 
-    x = math.floor(prop_latitude * shape[0])
-    y = math.floor(prop_longitude * shape[1])
+    x = shape[0] - math.floor(prop_latitude * shape[0])
+    y = shape[1] - math.floor(prop_longitude * shape[1])
 
-    return x, shape[1] - y
+    return x, y
