b9b9dfdb214c1a89259b646453a485850edfb79e
wiktorjakubowski <wjakubowski@deloittece.com>
2022-11-19 23:51:21 +0100

    added model

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import math
-
+from typing import Tuple
 # (21.9430, -67.5), (55.7765, -135)
 # (33.8335, 67.5)
 # shape 2566x5120
@@ -15,7 +15,7 @@ delta_longitude = BOUNDARIES[1][1] - BOUNDARIES[0][1]
 shape = (2565, 5119)  # 2566, 5120
 
 
-def coordinates_to_idx(latitude: int, longitude: int) -> tuple[int]:
+def coordinates_to_idx(latitude: int, longitude: int) -> Tuple[int]:
     prop_latitude = (latitude - BOUNDARIES[0][0]) / delta_latitude
     prop_longitude = (longitude - BOUNDARIES[0][1]) / delta_longitude
 
